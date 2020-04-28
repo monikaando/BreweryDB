5e867e0fd0c52142de3ca2ed5b7d540f278f5ba3
Monika <mswidzinska87@gmail.com>
2020-04-28 21:02:43 +0200

    layout of the homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,13 @@ import '../styles/Home.scss'
 export default function Home() {
     return (
         <div className="homepage">
-            <button>Breweries per country</button>
-            <button>Find a beer</button>
+            <div className="home-buttons">
+                <button>Breweries per country</button>
+                <button>Find a beer</button>
+            </div>
+            <div className="results">
+                <h1>Result:</h1>
+            </div>
         </div>
     )
 }
