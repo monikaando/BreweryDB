3be4c0a75e2a313301d76190c7f5cbae494906bd
Monika <mswidzinska87@gmail.com>
2020-04-30 22:06:51 +0200

    select countrycode

--- src/components/BreweryList.jsx
+++ src/components/BreweryList.jsx
@@ -5,24 +5,29 @@ import axios from 'axios';
 import _ from "lodash";
 
 
-
 class BreweryList extends Component {
     constructor(props) {
         super(props);
-        this.state={
-            breweries:[]
+        this.state = {
+             select: {
+                selectedCode:[]
+            },
+            breweries:[],
+            countryCode:[]
         }
         this.getBreweriesList=this.getBreweriesList.bind(this);
         this.removeDuplicates=this.removeDuplicates.bind(this);
+        this.getCountryCodeList=this.getCountryCodeList.bind(this);
+        this.handleInputChange=this.handleInputChange.bind(this);
     }
     componentDidMount() {
         this.getBreweriesList();
-        console.log(this.state.breweries)
+        this.getCountryCodeList(); 
     }
     getBreweriesList(){
         axios({
             method: "GET",
-            url: "http://localhost:3000/locations/?countryIsoCode=US&order=breweryName&key=659d5c6b8f3d2447f090119e48202fdb"
+            url: `http://localhost:3000/locations/?countryIsoCode${this.state.selectedCode}&order=breweryName&key=659d5c6b8f3d2447f090119e48202fdb`
         })
         .then(res => {
             this.setState({
@@ -31,7 +36,7 @@ class BreweryList extends Component {
             this.removeDuplicates()  
         })
         .catch((err)=> {
-                console.log( "Not sent")
+                console.log( "Error")
         })
     }
     removeDuplicates() {
@@ -42,13 +47,57 @@ class BreweryList extends Component {
             breweries:unique
         })
     }
+    getCountryCodeList(){
+        axios({
+            method: "GET",
+            url: "http://localhost:3000/locations/?key=659d5c6b8f3d2447f090119e48202fdb"
+        })
+        .then(res => {
+            let code = [...new Set(res.data.data.map(item => item.countryIsoCode))]
+            this.setState({
+                countryCode: code
+            })
+            console.log('All unique coutry codes in Array: '+ this.state.countryCode)
+        })
+        .catch((err)=> {
+                console.log( "Error")
+        })
+    }
+    handleInputChange(e) {
+        e.preventDefault();
+        let updatedCountryCode = this.state.select;
+        updatedCountryCode[e.target.name] = e.target.value;
+        this.setState({
+            select:updatedCountryCode
+        })
+        console.log(this.state.select.selectedCode)
+        this.props.history.push(`/breweries/`);
+
+    }
+
     render() {
         return (
             <div>
+            <div>
+            <select
+                multiple={true} type="select-multiple"
+                aria-label="country-code" 
+                name="selectedCode" 
+                value={this.state.select.selectedCode} 
+                onChange={this.handleInputChange}
+            >
+                <option value="">Choose a country</option>
+                {this.state.countryCode.map(item => (
+                  <option name="selectedCode" key={item} value={item}>
+                    {item}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <h2>Breweries from {this.state.select.selectedCode}</h2>
                 {this.state.breweries.map((item) => (
                     <div key={item.id}>
-                       <Link to={`brewery/${item.breweryId}`}> <h4>{item.brewery.name}</h4>
-                       <h4>{item.brewery.name}</h4></Link>
+                       <Link to={`brewery/${item.breweryId}`}> <h4>{item.brewery.name}</h4></Link>
                     </div>
                 ))}
             </div>    
