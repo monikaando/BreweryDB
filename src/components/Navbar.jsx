4b5626885f7651e8504f6f3b844a6768a91ba6e3
Monika <mswidzinska87@gmail.com>
2020-05-05 23:00:33 +0200

    mobile version

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,9 @@ export default function Navbar() {
     return (
         <div className="nav-buttons">
                     <Link to={"/brewery"} className="button">Breweries per country</Link>
+                    <div className="home">
                     <Link to={"/"}> <img src="/white-home-icon.png" alt="TapApp-homepage"/></Link>
+                    </div>
                     <Link to={"/brewery"} className="button">Find a beer</Link>
         </div>
     )
